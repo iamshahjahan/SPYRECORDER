3ce740ccf036f21cfd8a5dadc4a651ce7146297e
shiza1aas <ahmsjahan@gmail.com>
2016-04-04 20:54:09 +0100

    Need to work on ui design. Every thing else is working fine.

--- app/src/main/java/com/example/khan/spyrecorder/MainActivity.java
+++ app/src/main/java/com/example/khan/spyrecorder/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.khan.spyrecorder;
 
-import android.media.MediaPlayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.graphics.Bitmap;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -10,19 +14,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi.DriveContentsResult;
+import com.google.android.gms.drive.MetadataChangeSet;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import com.google.android.gms.drive.Drive;
-import com.google.api.client.http.FileContent;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.ParentReference;
+/**
+ * Android Drive Quickstart activity. This activity takes a photo and saves it
+ * in Google Drive. The user is prompted with a pre-made dialog which allows
+ * them to choose the file location.
+ */
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks,
+        OnConnectionFailedListener {
 
-public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
+    private static final int REQUEST_CODE_CREATOR = 2;
+    private static final int REQUEST_CODE_RESOLUTION = 3;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Bitmap mBitmapToSave;
+
 
     private static final String TAG = "Shahjahan";
     private MediaRecorder myAudioRecorder;
@@ -35,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("SPY RECORDER");
         setSupportActionBar(toolbar);
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.mp3";
 
         myAudioRecorder = new MediaRecorder();
 
@@ -47,10 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
         myAudioRecorder.setOutputFile(outputFile);
 
-
-
     }
-
 
     public void record(View view) {
         try {
@@ -65,55 +84,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void stop(View view) {
-        myAudioRecorder.stop();
-        myAudioRecorder.release();
-        myAudioRecorder  = null;
-        Log.d("TAG","Something went wrong while preparing the recorder");
-
-    }
-
-    public void play(View view) {
-        MediaPlayer m = new MediaPlayer();
-        try {
-            m.setDataSource(outputFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            m.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        m.start();
-        Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+       try
+       {
+           myAudioRecorder.stop();
+           myAudioRecorder.release();
+           myAudioRecorder  = null;
+           saveFileToDrive();
+       }
+       catch (Exception e)
+       {
+           Log.d("TAG", "Something went wrong while preparing the recorder");
+       }
+        Log.d("TAG", "Saving into the database.");
     }
 
     /**
      * Create a new file and save it to Drive.
      */
-    private void saveFiletoDrive(final File file, final String mime) {
+    private void saveFileToDrive() {
         // Start by creating a new contents, and setting a callback.
-        Drive.DriveApi.newDriveContents(mDriveClient).setResultCallback(
-                new ResultCallback<DriveContentsResult>() {
+        Log.i(TAG, "Creating new contents.");
+        Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                .setResultCallback(new ResultCallback<DriveContentsResult>() {
+
                     @Override
                     public void onResult(DriveContentsResult result) {
-                        // If the operation was not successful, we cannot do
-                        // anything
+                        // If the operation was not successful, we cannot do anything
                         // and must
                         // fail.
                         if (!result.getStatus().isSuccess()) {
                             Log.i(TAG, "Failed to create new contents.");
                             return;
                         }
-                        Log.i(TAG, "Connection successful, creating new contents...");
                         // Otherwise, we can write our data to the new contents.
+                        Log.i(TAG, "New contents created.");
                         // Get an output stream for the contents.
-                        OutputStream outputStream = result.getDriveContents()
-                                .getOutputStream();
+                        OutputStream outputStream = result.getDriveContents().getOutputStream();
+                        // Write the bitmap data from it.
                         FileInputStream fis;
                         try {
-                            fis = new FileInputStream(file.getPath());
+                            fis = new FileInputStream(outputFile);
                             ByteArrayOutputStream baos = new ByteArrayOutputStream();
                             byte[] buf = new byte[1024];
                             int n;
@@ -123,45 +133,91 @@ public class MainActivity extends AppCompatActivity {
                             outputStream.write(photoBytes);
 
                             outputStream.close();
-                            outputStream = null;
                             fis.close();
-                            fis = null;
 
                         } catch (FileNotFoundException e) {
                             Log.w(TAG, "FileNotFoundException: " + e.getMessage());
                         } catch (IOException e1) {
                             Log.w(TAG, "Unable to write file contents." + e1.getMessage());
                         }
-
-                        String title = file.getName();
+                        // Create the initial metadata - MIME type and title.
+                        // Note that the user will be able to change the title later.
                         MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
-                                .setMimeType(mime).setTitle(title).build();
+                                .setMimeType("audio/mpeg").setTitle(outputFile).build();
+                        // Create an intent for the file chooser, and start it.
+                        Drive.DriveApi.getRootFolder(mGoogleApiClient)
+                                .createFile(mGoogleApiClient, metadataChangeSet, result.getDriveContents())
+                        ;
 
-                        if (mime.equals(MIME_PHOTO)) {
-                            if (VERBOSE)
-                                Log.i(TAG, "Creating new photo on Drive (" + title
-                                        + ")");
-                            Drive.DriveApi.getFolder(mDriveClient,
-                                    mPicFolderDriveId).createFile(mDriveClient,
-                                    metadataChangeSet,
-                                    result.getDriveContents());
-                        } else if (mime.equals(MIME_VIDEO)) {
-                            Log.i(TAG, "Creating new video on Drive (" + title
-                                    + ")");
-                            Drive.DriveApi.getFolder(mDriveClient,
-                                    mVidFolderDriveId).createFile(mDriveClient,
-                                    metadataChangeSet,
-                                    result.getDriveContents());
-                        }
-
-                        if (file.delete()) {
-                            if (VERBOSE)
-                                Log.d(TAG, "Deleted " + file.getName() + " from sdcard");
-                        } else {
-                            Log.w(TAG, "Failed to delete " + file.getName() + " from sdcard");
-                        }
                     }
                 });
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+            // Create the API client and bind it to an instance variable.
+            // We use this instance as the callback for connection and connection
+            // failures.
+            // Since no account name is passed, the user is prompted to choose.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        // Connect the client. Once connected, the camera is launched.
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+
+                if (resultCode == Activity.RESULT_OK) {
+                    // Store the image data as a bitmap for writing later.
+                    Log.d(TAG,"Data is saved successfully.");
+                }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Called whenever the API client fails to connect.
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+        // The failure has a resolution. Resolve it.
+        // Called typically when the app is not yet authorized, and an
+        // authorization
+        // dialog is displayed to the user.
+        try {
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(TAG, "API client connected.");
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+    }
 }
